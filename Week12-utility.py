9b4f6f3d84611a43c4e3106713b590dc9bb95e46
Ty Ridings <tyridings@gmail.com>
2019-11-19 11:59:34 -0700

    Updated with UpdateString function

--- Week12-utility.py
+++ Week12-utility.py
@@ -9,3 +9,13 @@ def LoadFile(filename):
     with open(filename,'r+') as f:
         lines = f.readlines()
     print(lines)
+
+def UpdateString(string1, string2, num):
+    list1 = []
+    final = ''
+    for i in string1:
+        list1 += i
+    list1[num] = string2
+    for n in list1:
+        final += n
+    print(final)
